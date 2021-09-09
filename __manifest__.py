84f8f386a16dc0fa5441f5eb17b00eaef6c9651a
morrillo <morrillo@umich.edu>
2021-09-09 17:10:33 +0000

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'I2T - Capacitacion',
+    'category': 'Accounting',
+    'version': '1.1',
+    'depends': ['crm', 'base','sale','account'],
+    'data': [
+        'demo_view.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
